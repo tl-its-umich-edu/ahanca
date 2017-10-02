6d9e4197c29131ada5ef2b071c63fb1d433d77f0
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-10-02 13:58:43 -0400

    Switching jq to Lodash, etc.
    Changing from jq to Lodash for selecting applicable specs from the mapping.  This isn't complete yet.  The jq version had worked, but was not optimal.  Although I thought that using jq for everything (internally and in the spec mappings) would be nice, it's not necessary.  So I've switched to Lodash for the pieces that don't come from the mapping.  (As I've seen from an xAPI project, I may re-reconsider JSONPath for that instead of jq anyway.)  So far, I was just getting to know Lodash and it's not using the input yet.
    
    I've also homogenized the mapping JSON to always use an array for `domain` and expanded the README a bit.

--- ahanca.js
+++ ahanca.js
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const _ = require('lodash');
 const jq = require('node-jq');
 
-const argumentErrorMessage = 'Error: Exactly 2 arguments are required';
-const arguments = require('yargs')
-    .usage('Usage: $0 [options] <specFrom> <specTo>')
-    .command('specFrom', 'Specification version to be converted from (e.g., "caliper-1p0")') // first ARGUMENT
-    .command('specTo', 'Specification version to be converted to (e.g., "caliper-1p1")') // second ARGUMENT
-    .demandCommand(2, 2, argumentErrorMessage, argumentErrorMessage) // num. of req. ARGUMENTS (NOT commands)
+const argumentsRequired = {
+    specFrom: 'Specification version to be converted from (e.g., "caliper-1p0")',
+    specTo: 'Specification version to be converted to (e.g., "caliper-1p1")',
+    inputFilename: 'Name of the input file',
+};
+const argumentsRequiredNum = Object.keys(argumentsRequired).length;
+const argumentsRequiredError = 'Error: Exactly ' + argumentsRequiredNum + ' arguments are required';
+
+const argumentParser = require('yargs')
+    .usage('Usage: $0 [options] ' + '<' + Object.keys(argumentsRequired).join('> <') + '>');
+
+for (var argument in argumentsRequired) {
+    if (argumentsRequired.hasOwnProperty(argument)) {
+        console.log(argument + " -> " + argumentsRequired[argument]);
+        argumentParser.command(argument, argumentsRequired[argument]);
+    }
+}
+
+var arguments = argumentParser
+    .demandCommand(argumentsRequiredNum, argumentsRequiredNum,
+        argumentsRequiredError, argumentsRequiredError)
     .argv._;
 
-[specFrom, specTo] = arguments.slice(0, 2);
+var specFrom, specTo, inputFilename;
+[specFrom, specTo, inputFilename] = arguments.slice(0, argumentsRequiredNum);
 
 console.log('Starting ahanca...');
 console.log('Converting from: "%s".  Converting to: "%s"', specFrom, specTo);
@@ -25,23 +42,40 @@ console.log('Converting from: "%s".  Converting to: "%s"', specFrom, specTo);
 const filterPreamble = fs.readFileSync('xor.jq', 'utf8');
 const jqOptions = {input: 'json'};
 
-// Expected: List of terms that contain both specs in the *SAME* definition.
-// Actual: List of terms that contain both specs in *ANY* definition.
-// var filter = '.{(.mapping.spec == "' + specFrom + '") && (.mapping.spec == "' + specTo + '")}.term';
 var filter =
-    'map(.|select(.mapping[].spec|xor(contains(["' +
-    specFrom +
-    '"]); contains(["' +
-    specTo +
-    '"])))|{term, mapping: [.mapping[]|select(.spec|(contains(["' +
-    specFrom +
-    '"]) or contains(["' +
-    specTo +
-    '"])))]})'; // terms with specFrom value
+    '"' + specFrom + '" as $specFrom | ' +
+    '"' + specTo + '" as $specTo | ' +
+    'map(. | select(.mapping[].spec | xor(contains([$specFrom]); contains([$specTo]))) | ' +
+    '{term, mapping: [.mapping[] | select(.spec | (contains([$specFrom]) or contains([$specTo])))]})';
 var mapping = JSON.parse(fs.readFileSync('mapping.json', 'utf8'));
 
-// var filter = 'map(select(.letters | xor(contains(["a"]); contains(["b"]))))';
-// var mapping = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+// var terms = _(mapping).map(function (termMap) {
+//     return termMap.term
+// }).value()
+
+var terms = _(mapping)
+    .filter(function (t) {
+        return t.domain.includes('ahanca:root');
+    })
+    .map(function (termMap) {
+        return termMap.term
+    })
+    .value();
+
+console.dir(terms, {'depth': 9999});
+
+var specs = _(mapping)
+    .filter(function (t) {
+        return t.domain.includes('ahanca:root');
+    })
+    .map(function (termMap) {
+        return _.map(termMap, 'spec');
+    })
+    .value();
+
+console.dir(specs, {'depth': 9999});
+
+process.exit(999);
 
 jq.run(filterPreamble + filter, mapping, jqOptions).then(console.log);
 
